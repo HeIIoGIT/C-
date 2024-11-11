cb690b2f81f0a4c408806b67da018dfd6e80b620
=)  0_0 <138306149+HeIIoGIT@users.noreply.github.com>
2024-11-12 03:01:02 +0500

    Add files via upload

---
+++ SwichCalculatorSimpleCode.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+	setlocale(0, "");
+
+	int a, b;
+	char operation;
+	cout << "Введите два числа <int>\n";
+	cout << "первое число <a> : ";
+	cin >> a;
+	cout << "второе число <b> : ";
+	cin >> b;
+	cout << "\nВведите знак операции <+-*/>: ";
+	cin >> operation;
+	cout << endl;
+
+	switch (operation)
+	{
+	case '+':
+		cout << a << " + " << b << " = " << a + b << endl;
+		break;
+	case '-':
+		cout << a << " - " << b << " = " << a - b << endl;
+		break;
+	case '*':
+		cout << a << " * " << b << " = " << a * b << endl;
+		break;
+	case '/':
+		if (b != 0)
+		{
+			cout << a << " / " << b << " = " << a / b << endl;
+			cout << a << " % " << b << " = " << a % b << endl;
+		}
+		else
+		{
+			cerr << "НА НОЛЬ ДЕЛИТЬ НЕЛЬЗЯ!";
+		}
+		break;
+	default:
+		cout << "Я вообще без понятия!" << endl;
+		break;
+	}
+
+	return 0;
+}
